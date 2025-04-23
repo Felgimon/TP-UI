c5ebf996dd0c9bc446178e7a0f7a2792bb7faf0e
Felgi <felipeferraridoval@gmail.com>
2025-04-23 15:39:18 -0300

    Ingreso clave password

---
+++ Assets/ClaveManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ClaveManager : MonoBehaviour
+{
+    public TMP_InputField inputClave;
+    public TextMeshProUGUI txtMensaje;
+    string clave = "clave123";
+    void Start()
+    {
+        txtMensaje.text = string.Empty;
+    }
+
+    public void VerificarClave()
+    {
+       if (inputClave.text != clave)
+       {
+            txtMensaje.text = "Acceso denegado";
+            inputClave.text = string.Empty;
+            return;
+       } 
+       txtMensaje.text = "Facturas bro";
+    }
+}
